90ce853085df5cd9e3454f31e5e8763797756bbc
Edvá Lima <edvalima25@gmail.com>
2022-11-16 11:14:45 -0300

    Media Queries

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function cadastrarNewsletter(){
+   let email = document.getElementById("campo-email").value
+   alert(email)
+}
+
+function mostrarMenu($event){
+    let menu = document.getElementById("menu")
+
+    if(getComputedStyle(menu).display == "none"){
+        menu.style.display ="flex"
+    }
+    else{
+        menu.style.display="none"
+    }
+
+    event.preventDefault();
+}
