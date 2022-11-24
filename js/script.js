ff0c6563c8c31c449b54edcfd3cf0a1996e7ccf7
Edvá Lima <edvalima25@gmail.com>
2022-11-23 21:24:52 -0300

    Codificação 3

--- js/script.js
+++ js/script.js
@@ -1,3 +1,47 @@
+$(document).ready(function(){
+
+    let slideAtual = 1
+    let listaSlides = ["banner-f1", "banner-cs", "banner-lol"]
+
+    setInterval(mudarSlide, 2000);
+
+    function mudarSlide(){
+        //remover o slider anterior
+        if(slideAtual > 0){
+            $("#carrossel").removeClass(listaSlides[slideAtual -1])
+        }
+        else{
+            $("#carrossel").removeClass(listaSlides[2])
+        }
+
+        $("#carrossel").removeClass(listaSlides[slideAtual -1])
+        
+        //exibir o slide atual
+        $("#carrossel").addClass(listaSlides[slideAtual])
+        
+        //indicar qual o slider atual
+        console.log("sLIDE atual é: ",slideAtual)
+        slideAtual ++
+
+        if(slideAtual > 2){
+            slideAtual = 0
+        }
+    }
+
+
+    $("#barras").click(function(){
+        if ($("#menu").hasClass("menu-ativo")){
+            $("#menu").removeClass("menu-ativo")
+        }
+        else{
+            $("#menu").addClass("menu-ativo")
+        }
+    })
+})
+
+
+
+
 function cadastrarNewsletter(){
    let email = document.getElementById("campo-email").value
    alert(email)
